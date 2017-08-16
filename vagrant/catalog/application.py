eeb89bdb9f9269744186da8515b64bf51b0a9388
Wei Zhang <zhangwei1990@hotmail.com>
2017-08-16 22:38:38 +0800

    Show items of selected catalog

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -27,8 +27,20 @@ def home():
 	items = session.query(Item).order_by(desc(Item.date)).all()
 	catalogs_display = [{'id' : catalog.id, 'name' : catalog.name} for catalog in catalogs]
 	items_display = [{'id' : item.id, 'title' : item.title, 'catalog' : item.catalog.name} for item in items]
+	items_summary = 'Latest Items'
 	disconnect_db()
-	return render_template('home.html', catalogs_display = catalogs_display, items_display = items_display)
+	return render_template('home.html', catalogs_display = catalogs_display, items_display = items_display, items_summary = items_summary)
+
+@app.route('/catalog/<name>/')
+def get_catalog_items(name):
+	connect_db()
+	catalogs = session.query(Catalog).all()
+	items = session.query(Catalog).filter_by(name = name).one().items
+	catalogs_display = [{'id' : catalog.id, 'name' : catalog.name} for catalog in catalogs]
+	items_display = [{'id' : item.id, 'title' : item.title} for item in items]
+	items_summary = '{0} Items ({1} items)'.format(name, len(items_display))
+	disconnect_db()
+	return render_template('home.html', catalogs_display = catalogs_display, items_display = items_display, items_summary = items_summary)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
