74455ea8a3277e9ac7e68ff0a15afd12598f1028
Wei Zhang <zhangwei1990@hotmail.com>
2017-08-16 21:47:05 +0800

    Add home page

--- vagrant/catalog/setup_db.py
+++ vagrant/catalog/setup_db.py
@@ -1,50 +1,9 @@
 #!/usr/bin/env python
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, create_engine
-from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model import Base, Catalog, Item, create_catalog_with_items
 from data import fake_data
-
-Base = declarative_base()
-
-class Catalog(Base):
-	"""Define category schema"""
-
-	__tablename__ = 'catalog'
-
-	id = Column(Integer, primary_key=True)
-	name = Column(String)
-	date = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
-
-	items = relationship("Item", back_populates="catalog")
-
-	def __init__(self, name):
-		self.name = name
-
-class Item(Base):
-	"""Define item schema"""
-
-	__tablename__ = 'item'
-
-	id = Column(Integer, primary_key=True)
-	title = Column(String)
-	desc = Column(String)
-	date = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
-
-	catalog_id = Column(Integer, ForeignKey('catalog.id'))
-	catalog = relationship("Catalog", back_populates="items")
-
-	def __init__(self, title, desc):
-		self.title = title
-		self.desc = desc
-
-def create_catalog_with_items(catalog_name, items):
-	catalog = Catalog(catalog_name)
-	for item_name, item_desc in items.items():
-		catalog.items.append(Item(item_name, item_desc))
-
-	return catalog
 
 def setup_db():
 	# Create engine
