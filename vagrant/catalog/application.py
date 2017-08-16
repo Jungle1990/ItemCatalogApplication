1194bbe73d9a76116dde80b5e1ec3b3aff74e8fb
Wei Zhang <zhangwei1990@hotmail.com>
2017-08-16 23:12:06 +0800

    Show description of selected item

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -31,16 +31,25 @@ def home():
 	disconnect_db()
 	return render_template('home.html', catalogs_display = catalogs_display, items_display = items_display, items_summary = items_summary)
 
-@app.route('/catalog/<name>/')
-def get_catalog_items(name):
+@app.route('/catalog/<id>/')
+def get_catalog_items(id):
 	connect_db()
 	catalogs = session.query(Catalog).all()
-	items = session.query(Catalog).filter_by(name = name).one().items
+	selected_catalog = session.query(Catalog).filter_by(id = id).one()
+	items = selected_catalog.items
 	catalogs_display = [{'id' : catalog.id, 'name' : catalog.name} for catalog in catalogs]
 	items_display = [{'id' : item.id, 'title' : item.title} for item in items]
-	items_summary = '{0} Items ({1} items)'.format(name, len(items_display))
+	items_summary = '{0} Items ({1} items)'.format(selected_catalog.name, len(items_display))
 	disconnect_db()
 	return render_template('home.html', catalogs_display = catalogs_display, items_display = items_display, items_summary = items_summary)
+
+@app.route('/item/<id>')
+def get_item(id):
+	connect_db()
+	item = session.query(Item).filter_by(id = id).one()
+	item_display = {'id' : item.id, 'title' : item.title, 'desc' : item.desc}
+	disconnect_db()
+	return render_template('item_detail.html', item_display = item_display)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
